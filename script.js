7ad312a884ac5c88e326bfee852623deac12a5e4
oscardvs <59517821+oscardvs@users.noreply.github.com>
2024-03-28 12:01:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     setTimeout(() => {
-        document.getElementById('loading').classList.add('hide-loading');
-    }, 3000);
+        document.querySelector('.flashing-background').style.display = 'none';
+        document.querySelector('.loader').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 3000); // Adjust time as needed for your loading animation
 });
