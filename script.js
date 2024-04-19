fa9b644147d4f5e29a831e02923100ac9eae2eed
oscardvs <59517821+oscardvs@users.noreply.github.com>
2024-04-20 00:33:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    setTimeout(() => {
-        document.querySelector('.flashing-background').style.display = 'none';
-        document.querySelector('.loader').style.display = 'none';
-        document.getElementById('main-content').style.display = 'block';
-    }, 3000); // Adjust time as needed for your loading animation
-});
